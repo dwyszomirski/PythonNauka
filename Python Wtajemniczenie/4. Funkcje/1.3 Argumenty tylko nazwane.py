e7091a63b732f7b3b35dbf96933ff61c80e45f05
Dominik Wyszomirski <d.wyszomirski98@gmail.com>
2023-03-02 15:16:19 +0100

    Nauka

---
+++ Python Wtajemniczenie/4. Funkcje/1.3 Argumenty tylko nazwane.py
@@ -0,0 +1,24 @@
+def run_example():
+    loan_amount = int(input("Podaj wartosc kredytu: "))
+    property_value = int(input("Podaj wartosc nieruchomosci: "))
+
+    is_affordable = check_affordability(loan_amount, property_value)
+    is_affordable = check_affordability(loan_amount, property_value, max_ltv=0.9)
+    is_affordable = check_affordability(loan_amount, property_value, 0.9) #Bład
+    print_results(is_affordable)
+
+
+def check_affordability(loan_amount, property_value, *, max_ltv=0.8): #Wymuszamy uzycia arugmentu nazwanego
+    loan_to_value = loan_amount / property_value
+    return loan_to_value <= max_ltv
+
+
+def print_results(is_affordable):
+    if is_affordable:
+        print("Mozesz wziac kredyt!")
+    else:
+        print("Niesety nie mozesz wziac kredytu...")
+
+
+if __name__ == "__main__":
+    run_example()
